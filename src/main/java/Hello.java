e8091b4c566e0ccff935ade62c27e4922cacd9e8
Lea F <Lea Franz>
2019-03-23 15:02:23 +0100

    change of loop

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
         System.out.println("Hello World!");
         System.out.println("Git is fun");
-        for(int i=0; i<2; i++) {
+        for(int i=0; i<4; i++) {
             System.out.println("merge?");
         }
     }
