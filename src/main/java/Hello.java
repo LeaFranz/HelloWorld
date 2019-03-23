1a9c62ac7e22e9d326dd089e1b7f94516dcd886e
Lea F <Lea Franz>
2019-03-23 14:59:17 +0100

    new text and ln deleted

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main (String [] args){
 
         System.out.println("Hello World!");
-        System.out.println("Git is fun");
+        System.out.print("Git is fun");
+        System.out.println("merge?");
     }
 }
